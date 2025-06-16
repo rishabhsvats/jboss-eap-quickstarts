9d99cc5a78713841efb3646c747cd64bb7d39353
Rishabh Singh <rishabhsvats@gmail.com>
2025-06-16 12:09:05 +0530

    Update HelloWorldServlet.java

--- helloworld/src/main/java/org/jboss/as/quickstarts/helloworld/HelloWorldServlet.java
+++ helloworld/src/main/java/org/jboss/as/quickstarts/helloworld/HelloWorldServlet.java
@@ -47,7 +47,7 @@ public class HelloWorldServlet extends HttpServlet {
         resp.setContentType("text/html");
         PrintWriter writer = resp.getWriter();
         writer.println(PAGE_HEADER);
-        writer.println("<h1>Hello World!! This is a test</h1>");
+        writer.println("<h1>Hello World!! This is a test!</h1>");
         writer.println(PAGE_FOOTER);
         writer.close();
     }
